4d807db1535c756088e92d73b94701ba13252244
koatyann <mahihihi1127@gmail.com>
2024-06-06 10:47:39 +0900

    update

--- index.js
+++ index.js
@@ -19,7 +19,24 @@ init = async function () {
 
     // ウェブカメラをセットアップ
     const flip = true; // 画像を反転させるかどうか
-    webcam = new tmImage.Webcam(450, 300, flip); // カメラのサイズを指定
+
+    // カメラのセットアップオプションを取得
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const videoDevices = devices.filter(device => device.kind === 'videoinput');
+    let backCameraId = null;
+    videoDevices.forEach(device => {
+        if (device.label.toLowerCase().includes('back')) {
+            backCameraId = device.deviceId;
+        }
+    });
+
+    // バックカメラが見つかった場合、そのIDを使用してWebcamをセットアップ
+    if (backCameraId) {
+        webcam = new tmImage.Webcam(450, 300, flip, backCameraId);
+    } else {
+        webcam = new tmImage.Webcam(450, 300, flip); // バックカメラが見つからない場合、デフォルトカメラを使用
+    }
+    
     await webcam.setup(); // カメラをセットアップ
     await webcam.play(); // カメラを起動
 
